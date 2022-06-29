d5e02bf79b4bd482e65916dc34c02299295b1385
JArcher <viewtiful.kja@gmail.com>
2022-06-28 22:07:30 -0400

    Update time block text and moment.js format

--- Develop/script.js
+++ Develop/script.js
@@ -30,49 +30,60 @@ currentDayContainer.append(currentDay);
 
 //Determine Time of Day
 function timeOfDay() {
+    var workDayHours = [{ hr: 9, state: '' }, { hr: 10, state: '' }, { hr: 11, state: '' }, { hr: 12, state: '' }, { hr: 13, state: '' }, { hr: 14, state: '' }, { hr: 15, state: '' }, { hr: 16, state: '' }, { hr: 17, state: '' }];
+    var currentHour = moment().format('H');
+    for (var i in workDayHours) {
+        if (workDayHours[i].hr < currentHour) {
+            workDayHours[i].state = 'past'
 
+        } else if (workDayHours[i].hr = currentHour) {
+            workDayHours[i].state = 'present'
+        } else {
+            workDayHours[i].state = 'future'
+        }
+    }
+    return workDayHours;
 }
-
 
 
 //Create Time Block Rows
 function createTimeBlockRow() {
 
-
-    var tbRow = $('<div>').addClass('row time-block');
-    var tbHour = $('<div>').addClass('hour col-md-1').text('3');
-    var tbText = $('<textarea>').addClass('textarea past col-md-10');
-    var tbSave = $('<button>').addClass('saveBtn col-md-1');
-    var tbSaveIcon = $('<i>').addClass('material-icons').text('save');
+    var workHours = timeOfDay();
 
 
-    tbRow.appendTo(mainContainer);
-    tbHour.appendTo(tbRow);
-    tbText.appendTo(tbRow);
-    tbSave.appendTo(tbRow);
-    tbSaveIcon.appendTo(tbSave);
+    //console.log(workHours);
+
+    for (var i = 0; i < workHours.length; i++) {
+        var state = workHours[i].state;
+
+        //Coverts From Military Time
+        var hr = moment(workHours[i].hr, 'hh').format('h a');
 
 
+        //Assigning Element Creation as Variables
+        var tbRow = $('<div>').addClass(`row time-block ${state}`);
+        var tbHour = $('<div>').addClass('hour col-md-1').text(`${hr}`);
+        var tbText = $('<textarea>').addClass(`textarea col-md-10`);
+        var tbSave = $('<button>').addClass('saveBtn col-md-1');
+        var tbSaveIcon = $('<i>').addClass('material-icons').text('save');
 
-}
+        // Creating the HTML DOM Vertical
+        tbRow.appendTo(mainContainer);
+        tbHour.appendTo(tbRow);
+        tbText.appendTo(tbRow);
+        tbSave.appendTo(tbRow);
+        tbSaveIcon.appendTo(tbSave);
 
 
-
-
-
-
-
-
-
-
-
-
-//Generate Time Blocks
-function createTimeBlocks() {
-    for (var i = 0; i < 10; i++) {
-        createTimeBlockRow();
     }
+
 }
 
-// Build Time Blocks on Page Load
-createTimeBlocks();
+// Add Time Block Input Values to Local Storage
+/*$('.saveBtn').on('click', function (event) {
+    $(this).siblings()
+})
+
+// Build Time Blocks on Page Load*/
+createTimeBlockRow();
--- README.md
+++ README.md
@@ -6,7 +6,21 @@ I WANT to add important events to a daily planner
 SO THAT I can manage my time effectively
 
 1. WHEN I open the planner, THEN the current day is displayed at the top of the calendar
+    
     **Solution:** Create JS variable for Current day time and month and add to "currentDay" p tag.
 
 2. WHEN I scroll down, THEN I am presented with time blocks for standard business hours 
+   
     **Solution:** Use JS function to generate a grid of rows on page load. Each row (div) uses Bootstrap to style
+
+3. WHEN I view the time blocks for that day, THEN each time block is color-coded to indicate whether it is in the past, present, or future
+
+    **Solution:** Create a function that checks the current hours using moment.js and matches that against a map of hours that will hold the CSS value of either past, present, or future. That value will then return itself within another function that assigns the CSS class value upon element generation.
+
+4. WHEN I click into a time block, THEN I can enter an event
+
+    **Solution:** A textarea element is generated upon page load using a function
+
+5. WHEN I click the save button for that time block, THEN the text for that event is saved in local     storage
+
+    **Solution:** 
